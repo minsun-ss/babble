91cdb9442e7b50f2cba3e2e2569a3d2cbefbbb5f
minsun-ss <minsun@stuffofminsun.com>
2024-12-22 23:51:09 -0500

    feat: added a simple cleanup handler

--- handlers/clean.go
+++ handlers/clean.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"context"
+	"log"
 	"os"
 )
 
@@ -13,5 +15,32 @@ func NewCleanupFileHandler(tempFile *os.File) *CleanupFileHandler {
 	return &CleanupFileHandler{
 		TempFile: tempFile,
 		Done:     make(chan bool),
+	}
+}
+
+func (ch *CleanupFileHandler) MonitorContext(ctx context.Context) {
+	go func() {
+		select {
+		case <-ctx.Done():
+			log.Println("Client disconnected")
+		case <-ch.Done:
+			log.Println("Handler completed")
+		}
+	}()
+}
+
+func (ch *CleanupFileHandler) CleanupFile() {
+	defer func() {
+		ch.Done <- true
+	}()
+
+	if ch.TempFile != nil {
+		fileName := ch.TempFile.Name()
+		err := os.Remove(fileName)
+		if err != nil {
+			log.Printf("Error deleting temp file, may have already been deleted")
+		} else {
+			log.Printf("Temp file has been deleted")
+		}
 	}
 }
