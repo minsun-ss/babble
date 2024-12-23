573236131920e7b034fd7e224173a658b3c3bd4a
minsun-ss <minsun@stuffofminsun.com>
2024-12-23 16:47:30 -0500

    feat: removed the use of gorm.go

--- models/models.go
+++ models/models.go
@@ -1,4 +1,25 @@
+/*
+Package models contains all the models required for the application,
+including gorm-specific models. Gorm specific models are prefixed by the
+name DB.
+*/
 package models
+
+// gorm result for MenuListItem
+type DBMenuItem struct {
+	Name        string `gorm:"column:name"`
+	Description string `gorm:"column:description"`
+	Version     string `gorm:"column:version"`
+}
+
+type DBLibraryItem struct {
+	Description string `gorm:"column:description"`
+	Version     string `gorm:"column:version"`
+}
+
+type DBLibraryZip struct {
+	DataZip []byte `gorm:"column:html"`
+}
 
 // models for setting up the full library page
 type LibraryLink struct {
