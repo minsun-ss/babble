5bb8f86378e2fea6750dc46b6621b8432d5d768d
minsun-ss <minsun@stuffofminsun.com>
2024-12-24 15:19:09 -0500

    feat: updated zipfiles handler

--- handlers/zipfiles.go
+++ handlers/zipfiles.go
@@ -5,30 +5,57 @@ import (
 	"babel/config"
 	"babel/models"
 	"babel/utils"
+	"database/sql"
 	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func ServeZipFileHandler(db *config.DB) http.HandlerFunc {
+func DocsHandler(db *config.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// fetch information about the file
 		path := strings.TrimPrefix(r.URL.Path, "/docs/")
+
 		values := strings.Split(path, "/")
 		library, version := values[0], values[1]
+		values = strings.Split(version, ".")
+		major, err := strconv.Atoi(values[0])
+		if err != nil {
+			slog.Error("failed to parse library major version", "error", err, "path", path)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		minor, err := strconv.Atoi(values[1])
+		if err != nil {
+			slog.Error("failed to parse library minor version", "error", err, "path", path)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		patch, err := strconv.Atoi(values[2])
+		if err != nil {
+			slog.Error("failed to parse library patch version", "error", err, "path", path)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
 		prefix := "/docs/" + library + "/" + version + "/"
 		requestId := r.Header.Get(RequestIDHeader)
 
 		slog.Debug("configuration", "correlationId", requestId,
-			"path", path,
-			"library", library,
-			"version", version, "prefix", prefix)
+			"path", path, "library", library, "version", version,
+			"prefix", prefix, "major", major, "minor", minor, "patch", patch)
 
 		var dbZipResult models.DBLibraryZip
-		db.Raw(`select html from babel.doc_history
-			where name="traderpythonlib" and version_major="4"
-			and version_minor="0" and version_patch="0"`).Scan(&dbZipResult)
+		db.Raw(`SELECT html from babel.doc_history
+			WHERE name=@library and version_major=@major
+			and version_minor=@minor and version_patch=@patch`,
+			sql.Named("library", library),
+			sql.Named("major", major),
+			sql.Named("minor", minor),
+			sql.Named("patch", patch),
+		).Scan(&dbZipResult)
 
 		zipResult := dbZipResult.DataZip
 
