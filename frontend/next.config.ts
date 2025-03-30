b06bc0175e74e0fd7d1e55c968aa0fa9e5ad9f36
Min <minsun@stuffofminsun.com>
2025-03-30 11:23:47 -0400

    feat: updated title to the frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  assetPrefix: "/static/dist",
-  //Add trailingSlash to ensure proper routing for static exports
   trailingSlash: true,
   //Skip trailing slash redirect to prevent issues with asset paths
   skipTrailingSlashRedirect: true,
