8015e64735e0edaec95bd537534935ede9c25299
Min <minsun@stuffofminsun.com>
2025-04-18 13:22:57 -0400

    feat: adjusted the database api to create the users and api setup

--- schema/alembic/models/model.py
+++ schema/alembic/models/model.py
@@ -5,10 +5,43 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
-class Teams(Base):
-    __tablename__ = "teams"
+class User(Base):
+    __tablename__ = "users"
 
-    project_team = sa.Column(sa.String(50), nullable=False, primary_key=True)
+    id = sa.Column(sa.BigInteger, nullable=False, primary_key=True, autoincrement=True)
+    username = sa.Column(sa.String(50), nullable=False, unique=True)
+    iat = sa.Column(sa.Integer, nullable=False)
+    last_updated_dt = sa.Column(
+        mysql.TIMESTAMP,
+        nullable=False,
+        server_default=sa.text("current_timestamp() ON UPDATE current_timestamp()"),
+    )
+
+
+class UserAccess(Base):
+    __tablename__ = "user_access"
+
+    id = sa.Column(sa.BigInteger, nullable=False, primary_key=True, autoincrement=True)
+    username = sa.Column(sa.String(50), sa.ForeignKey("users.username", ondelete="CASCADE"), nullable=False)
+    project_name = sa.Column(sa.String(50), sa.ForeignKey("projects.project_name", ondelete="CASCADE"), nullable=False)
+    last_updated_dt = sa.Column(
+        mysql.TIMESTAMP,
+        nullable=False,
+        server_default=sa.text("current_timestamp() ON UPDATE current_timestamp()"),
+    )
+
+    __table_args__ = (
+        sa.UniqueConstraint("username", "project_name", name="uix_username_projectname"),
+        sa.Index("ix_username", username),
+        sa.Index("ix_project_name", project_name),
+        sa.Index("ix_last_updated_dt", last_updated_dt),
+    )
+
+
+class Projects(Base):
+    __tablename__ = "projects"
+
+    project_name = sa.Column(sa.String(50), nullable=False, primary_key=True)
     email = sa.Column(sa.String(50), nullable=True)
     last_updated_dt = sa.Column(
         mysql.TIMESTAMP,
@@ -16,47 +49,40 @@ class Teams(Base):
         server_default=sa.text("current_timestamp() ON UPDATE current_timestamp()"),
     )
 
-    __table_args__ = (sa.Index("ix_last_updated_dt", last_updated_dt),)
-
-
-class HashCheck(Base):
-    __tablename__ = "hashcheck"
-
-    # we are establishing one key per project team, which may also be associated with
-    # a project key. There are no admin keys this way; administration can be done via
-    # direct database manipulation instead of via api.
-    project_team = sa.Column(sa.String(50), nullable=False, primary_key=True)
-    hash = sa.Column(sa.String(100), nullable=False)
-    last_updated_dt = sa.Column(
-        mysql.TIMESTAMP,
-        nullable=False,
-        server_default=sa.text("current_timestamp() ON UPDATE current_timestamp()"),
+    __table_args__ = (
+        sa.Index("ix_project_name", project_name),
+        sa.Index("ix_last_updated_dt", last_updated_dt),
     )
-
-    __table_args__ = (sa.Index("ix_last_updated_dt", last_updated_dt),)
 
 
 class Docs(Base):
     __tablename__ = "docs"
 
-    name = sa.Column(sa.String(50), nullable=False, primary_key=True)
+    id = sa.Column(sa.BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    name = sa.Column(sa.String(50), nullable=False, unique=True)
     description = sa.Column(sa.String(50), nullable=True)
     is_visible = sa.Column(mysql.TINYINT, nullable=False, server_default=sa.text("1"))
-    project_team = sa.Column(sa.String(50), nullable=False, server_default=sa.text('"Other"'))
+    project_name = sa.Column(
+        sa.String(50), sa.ForeignKey("projects.project_name", ondelete="CASCADE"), server_default=sa.text('"Other"')
+    )
     last_updated_dt = sa.Column(
         mysql.TIMESTAMP,
         nullable=False,
         server_default=sa.text("current_timestamp() ON UPDATE current_timestamp()"),
     )
 
-    __table_args__ = (sa.Index("ix_last_updated_dt", last_updated_dt),)
+    __table_args__ = (
+        sa.Index("ix_name", name),
+        sa.Index("ix_project_name", project_name),
+        sa.Index("ix_last_updated_dt", last_updated_dt),
+    )
 
 
 class DocHistory(Base):
     __tablename__ = "doc_history"
 
     id = sa.Column(sa.BigInteger, autoincrement=True, nullable=False, primary_key=True)
-    name = sa.Column(sa.String(50), nullable=False)
+    name = sa.Column(sa.String(50), sa.ForeignKey("docs.name", ondelete="SET NULL"), nullable=False)
     version_major = sa.Column(sa.String(10), nullable=False)
     version_minor = sa.Column(sa.String(10), nullable=False)
     version_patch = sa.Column(sa.String(50), nullable=False)
@@ -68,9 +94,9 @@ class DocHistory(Base):
     )
 
     __table_args__ = (
-        sa.Index("idx_version_major", version_major),
-        sa.Index("idx_version_minor", version_minor),
-        sa.Index("idx_version_patch", version_patch),
-        sa.Index("idx_name", name),
+        sa.Index("ix_version_major", version_major),
+        sa.Index("ix_version_minor", version_minor),
+        sa.Index("ix_version_patch", version_patch),
+        sa.Index("ix_name", name),
         sa.UniqueConstraint(name, version_major, version_minor, version_patch, name="name"),
     )
