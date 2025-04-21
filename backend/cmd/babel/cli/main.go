65a556647876724ad569591cd43cd8db3b48eeee
Min <minsun@stuffofminsun.com>
2025-04-20 22:52:04 -0400

    feat: updated the cli and updated the move of the assets and all other files in /babel

---
+++ backend/cmd/babel/cli/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"babel/backend/internal/auth"
+	"babel/backend/internal/config"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	logLevel slog.Level
+)
+
+// Init sets up the logging function and customizes verbosity as needed
+func init() {
+	logLevel = slog.LevelDebug
+
+	textHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true, // Adds source file and line number
+	})
+
+	logger := slog.New(textHandler)
+	slog.SetDefault(logger)
+}
+
+func main() {
+	cfg := config.NewConfig()
+
+	app := &cli.App{
+		Name: "babel",
+		Before: func(ctx *cli.Context) error {
+			ctx.App.Metadata["config"] = cfg
+			return nil
+		},
+		Usage: "Handle authentication and management of babel user api",
+		Commands: []*cli.Command{
+			{
+				Name:  "create",
+				Usage: "Options for updating the API",
+				Action: func(ctx *cli.Context) error {
+					return cli.ShowSubcommandHelp(ctx)
+				},
+				Subcommands: []*cli.Command{
+					{
+						Name:  "user",
+						Usage: "add a new user",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "username",
+							},
+							&cli.StringFlag{
+								Name: "role",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							username := ctx.String("username")
+							if username == "" {
+								fmt.Println("username must be specified, exiting")
+								return fmt.Errorf("error in using create user")
+							}
+							role, err := auth.STRole(ctx.String("role"))
+							if err != nil {
+								slog.Error("There is no data for this item!")
+							}
+
+							apikey, err := auth.CreateUser(cfg.DB, cfg.Cfg.GetString("API_PRIVATE_KEY"), username, role)
+							if err != nil {
+								return fmt.Errorf("there was an error in creating an account")
+							} else {
+								fmt.Printf("key: %s", apikey)
+							}
+							return nil
+						},
+					},
+					{
+						Name:  "project",
+						Usage: "add a new project name",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "project",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							project := ctx.String("project")
+							if project == "" {
+								fmt.Println("project must be specified, exiting")
+								os.Exit(1)
+							}
+
+							auth.CreateProject(cfg.DB, project)
+							return nil
+						},
+					},
+				},
+			},
+			{
+				Name:  "delete",
+				Usage: "Options for removal from the API",
+				Action: func(ctx *cli.Context) error {
+					return cli.ShowSubcommandHelp(ctx)
+				},
+				Subcommands: []*cli.Command{
+					{
+						Name:  "user",
+						Usage: "delete an existing user",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "username",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							username := ctx.String("username")
+							if username == "" {
+								fmt.Println("a username must be specified, exiting")
+								os.Exit(1)
+							}
+
+							auth.DeleteUser(cfg.DB, username)
+							return nil
+						},
+					},
+					{
+						Name:  "project",
+						Usage: "delete an existing project name",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "project",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							project := ctx.String("project")
+							if project == "" {
+								fmt.Println("a project must be specified, exiting")
+								os.Exit(1)
+							}
+
+							return auth.DeleteProject(cfg.DB, project)
+						},
+					},
+				},
+			},
+			{
+				Name:  "access",
+				Usage: "Options for adding or removing access",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "grant",
+						Usage: "add project privilege to a user",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "username",
+							},
+							&cli.StringFlag{
+								Name: "project",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							username := ctx.String("username")
+							project := ctx.String("project")
+
+							if username == "" || project == "" {
+								fmt.Println("a username and project must be specified in the grant, exiting")
+								os.Exit(1)
+							}
+
+							return auth.GrantProjectAccess(cfg.DB, username, project)
+						},
+					},
+					{
+						Name:  "revoke",
+						Usage: "removing a project privilege to a user",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "username",
+							},
+							&cli.StringFlag{
+								Name: "project",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							username := ctx.String("username")
+							project := ctx.String("project")
+
+							if username == "" || project == "" {
+								fmt.Println("a username and project must be specified in the grant, exiting")
+								os.Exit(1)
+							}
+
+							return auth.RevokeProjectAccess(cfg.DB, username, project)
+						},
+					},
+					{
+						Name:  "retrieve",
+						Usage: "retrieving an api key for a user",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name: "username",
+							},
+						},
+						Action: func(ctx *cli.Context) error {
+							username := ctx.String("username")
+
+							apikey, err := auth.RetrieveAPIKey(cfg.DB, cfg.Cfg.GetString("API_PRIVATE_KEY"), username)
+							fmt.Printf("Key: %s", apikey)
+							return err
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ backend/cmd/babel/server/webserver.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"babel/backend/internal/api"
+	"babel/backend/internal/config"
+	"babel/backend/internal/handlers"
+
+	"io/fs"
+	"log"
+	"log/slog"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humago"
+)
+
+// Webserver sets up the webserver and sets up appropriate handlers
+func Webserver(config *config.Config) {
+	mux := http.NewServeMux()
+
+	// static files require moving moving down a subfolder to be
+	// appropriately referenced - this endpoint is to serve the internal
+	// frontend
+	static, err := fs.Sub(config.BabelFS, "assets")
+	if err != nil {
+		// for this particular error, yes, full webserver failure preferred
+		log.Fatal("static assets embedding failed:", err)
+	}
+	fs := http.FileServer(http.FS(static))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// http endpoints - some are semi deprecated but are here for reasons
+	mux.HandleFunc("/", handlers.IndexHandler(config.DB, config.BabelFS))
+	mux.HandleFunc("/info/", handlers.InfoHandler(config.DB, config.BabelFS))
+
+	// endpoints passed through to front end without handling
+	mux.HandleFunc("/libraries/", handlers.DocsHandler(config.DB))
+
+	// frontend endpoints - these are specifically for the frontend to use
+	mux.HandleFunc("/internal/menu/", handlers.IndexMenuHandler(config.DB))
+	mux.HandleFunc("/internal/links/", handlers.LibraryLinksHandler(config.DB))
+
+	// Create a Huma API with the HTTP adapter & register endpoints
+	babelAPI := humago.New(mux, *config.ApiCfg)
+	babelAPIGroup := huma.NewGroup(babelAPI, "/api/v1")
+	huma.Register(babelAPIGroup, api.ListLibrariesOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.PostLibrariesOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.GetLibraryOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.GetLibraryVersionOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.PatchLibraryOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.PatchLibraryVersionOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.DeleteLibraryOperation(), handlers.APIListHandler)
+	huma.Register(babelAPIGroup, api.DeleteLibraryVersionOperation(), handlers.APIListHandler)
+
+	// liveness check & prometheus
+	mux.HandleFunc("/healthz", handlers.LivenessHandler(config.DB))
+	mux.Handle("/metrics", handlers.MetricsHandler())
+
+	middlewareMux := handlers.NewMiddlewareHandler(mux)
+
+	// attempting to serve on 80
+	slog.Info("Starting webserver...", "port", 80)
+	go func() {
+		err := http.ListenAndServe(":80", middlewareMux)
+		if err != nil {
+			log.Fatal("HTTP server error:", err)
+		}
+	}()
+
+	select {}
+}
